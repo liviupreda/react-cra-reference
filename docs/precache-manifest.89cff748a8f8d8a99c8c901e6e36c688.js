4eb1d84527cdec2b2ee27bcbd31852b032b6b5eb
Liviu Preda <liviupreda@gmail.com>
2019-10-28 12:32:37 +0200

    cra initial commit

---
+++ docs/precache-manifest.89cff748a8f8d8a99c8c901e6e36c688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4349dc81e485bf0d39d3a153ec30d95a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e87dc3cbbd78e8b5e0c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5db253f6696211422593",
+    "url": "/static/js/2.faaa3ed1.chunk.js"
+  },
+  {
+    "revision": "5e87dc3cbbd78e8b5e0c",
+    "url": "/static/js/main.471d7797.chunk.js"
+  },
+  {
+    "revision": "71880945e581cb94d257",
+    "url": "/static/js/runtime-main.262f510d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
